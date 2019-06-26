3f39bbfd2d4a16c4d46ad478f1e553f45d5199d5
Jorgi Leus <Jorgi.Leus@axxes.com>
2019-06-26 12:08:33 +0200

    Add serviceWorker

---
+++ src/pokedex-serviceworker.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (event) => {
+    console.log('Inside the install handler:', event);
+  });
+  
+  self.addEventListener('activate', (event) => {
+    console.log('Inside the activate handler:', event);
+  });
--- src/register.js
+++ src/register.js
@@ -1,5 +1,11 @@
 const register = (swUrl, config) => {
-
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+            navigator.serviceWorker.register('/pokedex-serviceworker.js')
+                .then(console.log("Succes"))
+                .catch(console.log("Failure"));
+        });
+    }
 }
 
 export default register;
