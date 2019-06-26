5c5c66280af737e826770fac6b8756d4626c64c5
Jorgi Leus <Jorgi.Leus@axxes.com>
2019-06-26 12:12:44 +0200

    change register phase

--- src/register.js
+++ src/register.js
@@ -2,8 +2,12 @@ const register = (swUrl, config) => {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function () {
             navigator.serviceWorker.register('/pokedex-serviceworker.js')
-                .then(console.log("Succes"))
-                .catch(console.log("Failure"));
+                .then(function (registration) {
+                    console.log("Succes, scope: ", registration.scope)
+                })
+                .catch(function (err) {
+                    console.log("Failure, err; ", err)
+                });
         });
     }
 }
